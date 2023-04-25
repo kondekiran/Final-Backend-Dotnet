622926ee887594f3b35dc18a9e427297c80d6882
kondekiran <110444026+kondekiran@users.noreply.github.com>
2023-04-25 11:23:10 +0530

    Update Program.cs

--- EshopApplication/Program.cs
+++ EshopApplication/Program.cs
@@ -13,31 +13,47 @@ builder.Services.AddDbContext<EshopDB>(options => options.UseSqlServer(builder.C
 
 builder.Services.AddScoped<IUsers, UsersServices>();
 builder.Services.AddScoped<ICategories, CategoriesServices>();
-builder.Services.AddScoped<IProducts,ProductServices>();
+builder.Services.AddScoped<IProducts, ProductServices>();
+// builder.Services.AddScoped<ICart, CartServices>();
 builder.Services.AddScoped<IOrders, OrdersServices>();
 builder.Services.AddScoped<ICarts, CartsServices>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: "CorsPolicy", builder =>
+    {
+        builder.WithOrigins("")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
+app.UseSwagger();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseCors(x => x
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            );
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowAngularApp");
 
 app.MapControllers();
 
